da28eb6ee59de52767aebfac46dbf3db3be32eab
kimtaehun <kth_markany@naver.com>
2024-12-06 17:01:40 +0900

    svg 사용 처리 및 기본컴포넌트 생성

--- src/pages/loginPage/index.tsx
+++ src/pages/loginPage/index.tsx
@@ -1,13 +1,16 @@
 import AppWrapper from "@shared/components/Layout/AppWrapper";
-import { ReactComponent as UserIcon } from 'assets/user-solid.svg';
+import  UserIcon from 'assets/user-solid.svg';
 
 export default function LoginPage(){
   return (
     <AppWrapper>
       <div className="login-wrap">
         <div className="thumbnail">
-          <div>
-            <UserIcon width={32} height={32}/>
+          <div style={{
+            color: "white"
+          }}>
+            {/* <UserIcon width={32} height={32}/> */}
+            <img width={"32px"} src={UserIcon} alt="" />
             <h3>Login</h3>
           </div>
         </div>
