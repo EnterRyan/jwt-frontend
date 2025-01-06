9f26fdd45cf0fe4a97c3ca825a51759269a95e8f
kimtaehun <kth_markany@naver.com>
2025-01-06 17:05:08 +0900

    로그인 post함수 수정. api결과실패시 message가 디폴드로 찍혀서 수정필요

--- src/features/auth/api/LoginApi.ts
+++ src/features/auth/api/LoginApi.ts
@@ -2,14 +2,24 @@ import axios from "axios";
 import { LoginFormData, LoginUserData } from "../types/LoginData";
 
 export async function PostLogin(loginData: LoginFormData): Promise<LoginUserData | undefined> {
-  const URL = `${import.meta.env.VITE_API_URL}/login`;
+  const URL = `${import.meta.env.VITE_API_URL}/auth/login`;
+  console.log(URL);
 
   try {
-    const response = await axios.post<LoginUserData>(URL, loginData);
-    console.log(response.data);
+    // headers를 세 번째 인자로 전달
+    const response = await axios.post<LoginUserData>(
+      URL,
+      loginData,
+      {
+        headers: {
+          "Content-Type": "application/json", // 헤더 추가
+        },
+      }
+    );
+    console.log(response);
     return response.data;
   } catch (error) {
-    console.error("PostLogin Error:", error); 
-    return undefined; 
+    console.log(error);
+    return undefined;
   }
 }
